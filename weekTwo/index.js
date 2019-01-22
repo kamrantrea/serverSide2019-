40e21e2a5f9f7f38dc6807c3c9fedf0fe333f31c
Kamran Trea <kamran.trea@dcmail.ca>
2019-01-22 11:01:49 +0000

    init commit

---
+++ weekTwo/index.js
@@ -0,0 +1,16 @@
+// console.log(234 == '234');
+// console.log(234 === '234');
+// console.log(234234.235235 == 'cat');
+// console.log("cat" == "CAT");
+// console.log("cat".toUpperCase() == "CAT");
+// console.log(typeof "cat");
+// console.log(8*8);
+// console.log(parseInt("234.12345"));
+// console.log(parseInt("cat"));
+// isNaN(parseInt("cat"));
+// var i  = "this is a test".indexOf("is");
+// console.log(i);
+// var s = "this is a test strong.".substr(19,3);
+// var s1 = "this is a test string".slice(19,22);
+// console.log(s);
+// console.log(s1);
