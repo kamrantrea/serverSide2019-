58882f740f3964b74fd131fd6d512f7f5e2c66bd
Kamran Trea <kamran.trea@dcmail.ca>
2019-02-21 22:02:57 +0000

    mySQL and nodeJs

---
+++ tutorial/weekFive/routes/index.js
@@ -0,0 +1,20 @@
+/*HERE WERE GETTING THE DATA FROM THE DATABASE
+THAT WE ARE GOING TO DISPLAY*/
+var express = require('express')
+var app = express()
+app.get('/', function(req, res){
+    // res.send("hey")
+    req.getConnection(function(error,connection){
+
+        connection.query(`SELECT * FROM users ORDER BY id DESC`, function(err, rows, fields){
+            if (err){
+                res.send(err);
+                console.log(err);
+            }else{
+                res.send(rows);
+            }
+            });
+        });
+    });
+//EXTREMELY NECESSARY , SETS A CONNECTION BETWEEN OTHER FILES
+module.exports = app;
